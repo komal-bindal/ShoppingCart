24be27f841a34e2bdb3a3926ebdd2bafedfee7bb
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-09-25 16:42:11 +0530

    fix bug

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const express = require("express");
-
-const path = require("path");
+const express = require('express')
+const path = require('path')
 
 const app = express();
 
-app.use(express.json()); //for post request
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.use(express.urlencoded({extended: true}))//for post req
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/api', require('./route/api').route)
 
-app.use("/", express.static(path.join(__dirname, "public")));
-
-app.use("/api", require("./route/api").route);
-
-app.listen(2678, () =>
-  console.log("server started at http://localhost:2678")
-);
+app.listen(2678, () => console.log('Server started at http://localhost:2678'))
