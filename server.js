be4cfe09563511dac224da51fdaa5bcf351d7ce6
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-09-25 16:10:20 +0530

    fix error

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+
 const path = require("path");
+
 const app = express();
+
 app.use(express.json()); //for post request
-app.use(express.urlencoded((extended = true))); //for post req
+
+app.use(express.urlencoded({extended: true}))//for post req
+
 app.use("/", express.static(path.join(__dirname, "public")));
-app.use("/api", require("./route/api"));
-app.listen("2678", () =>
+
+app.use("/api", require("./route/api").route);
+
+app.listen(2679, () =>
   console.log("server started at http://localhost:2678")
 );
