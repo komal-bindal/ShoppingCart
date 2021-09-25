f07cccc4907978240c254f4c40ef2a82cb532d50
Komal Bindal <komal.bindal_cs19@gla.ac.in>
2021-09-25 16:18:23 +0530

    add products api

--- route/api/products.js
+++ route/api/products.js
@@ -1,2 +1,30 @@
 const route = require('express').Router();
+const Product  = require("../../db").Product
+route.get('/',(req, res)=>{
+    Product.findAll().then((products)=>{
+        res.status(200).send(products)
+    }).catch((error)=>{
+        res.status(500).send({
+            error: "Can not retrieve products"
+        })
+    })
+})
+route.post('/',(req, res)=>{
+    if(isNaN(req.body.price)){
+        return res.status(403).send({
+            error:"price is not valid number"
+        })
+    }
+    Product.create({
+        name: req.body.name,
+        manufacturer: req.body.manufacturer,
+        price: parseFloat(req.body.price)
+    }).then((product)=>{
+        res.status(201).send(product)
+    }).catch((error)=>{
+            res.status(501).send({
+                error:"can not add product"
+            })
+    })
+})
 exports = module.exports= route
--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/api", require("./route/api").route);
 
-app.listen(2679, () =>
+app.listen(2678, () =>
   console.log("server started at http://localhost:2678")
 );
